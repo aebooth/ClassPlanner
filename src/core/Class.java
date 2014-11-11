5bd5eb01f603c72cea777975545768f08bb98080
alex <alex@Booth-Arch.localdomain>
2014-11-10 18:12:32 -0800

    Started getting the Class object setup so that it would actually check for and create paths/directories.

--- src/core/Class.java
+++ src/core/Class.java
@@ -8,13 +8,18 @@ package core;
 
 import java.util.ArrayList;
 import java.io.File;
-import java.time.Instant;
+import java.sql.Timestamp;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 /**
  *
  * @author alex
  */
 public class Class implements Serializable {
+    private String classDirectory;
     private ArrayList<Unit> units;
     private ArrayList<String> notes;
     private String name;
@@ -22,13 +27,29 @@ public class Class implements Serializable {
     private String currentVersion;
     private ArrayList<File> referenceFiles;
     private ArrayList<File> worksheetFiles;
-    private Instant lastEdited;
-    
+    private Timestamp lastEdited;
+      
     public Class(){
-        this.units = new ArrayList<Unit>();
-        this.notes = new ArrayList<String>();
-        this.worksheetFiles = new ArrayList<File>();
-        this.referenceFiles = new ArrayList<File>();
+        this(checkDirectoryPath("NewClass"));
+    }
+      
+    public Class(String classDirectory){
+        this.classDirectory = classDirectory;
+        //TODO: Make this All kinds of file-friendly...
+        this.units = new ArrayList<>();
+        this.notes = new ArrayList<>();
+        this.worksheetFiles = new ArrayList<>();
+        this.referenceFiles = new ArrayList<>();
+        this.lastEdited = new Timestamp(System.currentTimeMillis());
+    }
+    
+    private static String checkDirectoryPath(String path)
+    {
+        int n = 1;
+        while(Files.exists(Paths.get(path),LinkOption.NOFOLLOW_LINKS)){
+            path = path + n;
+        }
+        return path;
     }
     
     public String getName(){
@@ -37,18 +58,22 @@ public class Class implements Serializable {
     
     public void setName(String name){
         this.name = name;
+        updateTimeStamp();
     }
     
     public void addVersion(String version){
         this.versions.add(version);
+        updateTimeStamp();
     }
     
     public void removeVersion(int index){
         this.versions.remove(index);
+        updateTimeStamp();
     }
     
     public void removeVersion(String version){
         this.versions.remove(version);
+        updateTimeStamp();
     }
     
     public void setVersion(String version){
@@ -57,10 +82,25 @@ public class Class implements Serializable {
             this.versions.add(version);
         }
             this.currentVersion = version;
+            updateTimeStamp();
     }
     
     public String getCurrentVersion(){
         return this.currentVersion;
+    }
+
+    public void addUnit(Unit unit){
+        this.units.add(unit);
+        
+    }
+    
+    public void removeUnit(Unit unit){
+        
+    }
+    
+    private void updateTimeStamp()
+    {
+        this.lastEdited = new Timestamp(System.currentTimeMillis());
     }
 
 }
