ceb0d6142268793e461d98596189c2dcff81521f
aebooth <mr.a.e.booth@gmail.com>
2015-07-10 01:35:21 -0700

    Thinking is hard, why not make a flowchart to help me write code. The XML file can be open @ draw.io/

--- db.py
+++ db.py
@@ -1,5 +1,11 @@
 import sqlite3
 import json
 
-def initialize_plan_json(filename):
-    pass
+def initialize_db_json(filename):
+    data = json.load(filename)
+    if "name" in data:
+        name = data[name]
+    else:
+        i = 1
+        while os.getcwd()+"/"+"class"+str(i) in os.listdir():
+            pass 
